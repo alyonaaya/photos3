8279d17b1f6a82dbf9c2170edce57c83620e0170
aayarulina <voronina.vt@gmail.com>
2019-10-03 21:16:53 +0300

    Add error handler Add tcp-connection reusage Add Semaphore to limit coroutine resources

--- photo.py
+++ photo.py
@@ -27,10 +27,32 @@ C_LIST = "list"
 C_UPLOAD = "upload"
 commands = [C_DOWNLOAD, C_GET, C_LIST, C_UPLOAD]
 
-# Данные для авторизации
-access_key = ""
-secret_key = ""
+
+"""
+Конфигурация
+"""
+# Адрес хранилища
 storage_url = 'http://localhost:9000'
+# Число одновременно запущенных корутин
+concurrency = 10
+
+
+def error_handler(f):
+    """
+    Обработчик ошибок
+    :param f: Асинхронная функция
+    :return: Пытается выполнить, иначе обработать возникшую ошибку
+    """
+    async def wrapper(*args, **kwargs):
+        try:
+            return await f(*args, **kwargs)
+        except ClientError as ce:
+            sys.exit(ce.response["Error"]["Message"])
+        except FileNotFoundError as fne:
+            sys.exit(str(fne))
+        except ValueError as ve:
+            sys.exit(str(ve))
+    return wrapper
 
 
 """
@@ -40,114 +62,87 @@ storage_url = 'http://localhost:9000'
 """
 
 
-async def upload_one_file(src, dest, bucket, loop):
+async def upload_one_file(src, dest, bucket, client, sem=asyncio.Semaphore(1)):
     """
     Функция загружает один файл в хранилище
     :param src: Путь до файла в фаловой система
     :param dest: Путь до файла в бакете
     :param bucket: Название бакета
-    :param loop:
+    :param client:
+    :param sem:
     :return:
     """
-    session = aiobotocore.get_session(loop=loop)
-    async with session.create_client("s3",
-                                     endpoint_url=storage_url,
-                                     region_name='us-west-2',
-                                     aws_secret_access_key=secret_key,
-                                     aws_access_key_id=access_key) as client:
+    async with sem:
         resp = await client.put_object(Bucket=bucket, Key=dest, Body=open(src, "rb"))
         print("{} uploaded".format(src))
 
 
-async def download_one_file(src, dest, bucket, loop):
+async def download_one_file(src, dest, bucket, client, sem=asyncio.Semaphore(1)):
     """
     Функция скачивает однин файл
     :param src: Путь до файла в бакете
     :param dest: Путь, куда будет сохранён файл
     :param bucket: Название бакета
-    :param loop:
+    :param client:
+    :param sem
     :return:
     """
-    session = aiobotocore.get_session(loop=loop)
-    async with session.create_client("s3",
-                                     endpoint_url=storage_url,
-                                     region_name='us-west-2',
-                                     aws_secret_access_key=secret_key,
-                                     aws_access_key_id=access_key) as client:
-        try:
-            resp = await client.get_object(Bucket=bucket, Key=src)
-        except ClientError as ce:
-            sys.exit(ce.response["Error"]["Message"])
+    async with sem:
+        resp = await client.get_object(Bucket=bucket, Key=src)
         async with resp["Body"] as stream:
             data = await stream.read()
-            with open(dest, "wb") as out_file:
+            out_file_name = os.path.join(dest, os.path.basename(src))
+            with open(out_file_name, "wb") as out_file:
                 out_file.write(data)
-            print("{} downloaded to {}".format(src, dest))
+            print("{} downloaded to {}".format(src, out_file_name))
 
 
-async def list_bucket(bucket, loop):
+async def list_bucket(bucket, client):
     """
     Функция для просмотра файлов в бакете
     :param bucket: Название бакета
-    :param loop:
+    :param client:
     :return:
     """
-    session = aiobotocore.get_session(loop=loop)
-    async with session.create_client("s3",
-                                     endpoint_url=storage_url,
-                                     region_name='us-west-2',
-                                     aws_secret_access_key=secret_key,
-                                     aws_access_key_id=access_key) as client:
-
-        try:
-            resp = await client.list_objects(Bucket=bucket)
-        except ClientError as ce:
-            sys.exit(ce.response["Error"]["Message"])
-        for obj in resp["Contents"]:
-            owner_name = obj["Owner"]["DisplayName"]
-            owner_name = "unknown" if len(owner_name) == 0 else owner_name
-            owner_id = obj["Owner"]["ID"]
-            dt = obj["LastModified"]
-            key = obj["Key"]
-            print("{} {} {} {}".format(owner_name, owner_id, dt.strftime("%Y-%m-%d %H:%M:%S %Z"), key))
+    resp = await client.list_objects(Bucket=bucket)
+    for obj in resp["Contents"]:
+        owner_name = obj["Owner"]["DisplayName"]
+        owner_name = "unknown" if len(owner_name) == 0 else owner_name
+        owner_id = obj["Owner"]["ID"]
+        dt = obj["LastModified"]
+        key = obj["Key"]
+        print("{} {} {} {}".format(owner_name, owner_id, dt.strftime("%Y-%m-%d %H:%M:%S %Z"), key))
 
 
-async def download_bucket(bucket, loop):
+async def download_bucket(bucket, client):
     """
     Функция для скачивания всего бакета в текущую директорию.
     Для параллельного скачивания файлов запускает внутренний асинхронный цикл
     :param bucket: Название бакета
-    :param loop:
+    :param client:
     :return:
     """
     download_tasks = list()
-    session = aiobotocore.get_session(loop=loop)
-    async with session.create_client("s3",
-                                     endpoint_url=storage_url,
-                                     region_name='us-west-2',
-                                     aws_secret_access_key=secret_key,
-                                     aws_access_key_id=access_key) as client:
 
-        # Сначала формируется список файлов, содержащхся в бакете
-        try:
-            resp = await client.list_objects(Bucket=bucket)
-        except ClientError as ce:
-            sys.exit(ce.response["Error"]["Message"])
-        for obj in resp["Contents"]:
-            directories = [bucket]
-            outdir = ""
-            key = obj["Key"]
-            path = key.split("/")
-            directories.extend(path[:-1])
-            for directory in directories:
-                outdir = os.path.join(outdir, directory)
-                if not os.path.exists(outdir):
-                    os.mkdir(outdir)
-            out_file_name = os.path.join(outdir, os.path.basename(key))
-            # На основе списка файлов из хранилища формируются задания на скачивание файлов по одному
-            download_tasks.append(asyncio.ensure_future(download_one_file(key, out_file_name, bucket, loop)))
+    # Семафор ограничивает потребляемые ресурсы
+    sem = asyncio.Semaphore(concurrency)
+
+    # Сначала формируется список файлов, содержащхся в бакете
+    resp = await client.list_objects(Bucket=bucket)
+    for obj in resp["Contents"]:
+        directories = [bucket]
+        outdir = ""
+        key = obj["Key"]
+        path = key.split("/")
+        directories.extend(path[:-1])
+        for directory in directories:
+            outdir = os.path.join(outdir, directory)
+            if not os.path.exists(outdir):
+                os.mkdir(outdir)
+        # На основе списка файлов из хранилища формируются задания на скачивание файлов по одному
+        download_tasks.append(asyncio.ensure_future(download_one_file(key, outdir, bucket, client, sem)))
         # Внутренний асинхронный цикл
-        await asyncio.wait(download_tasks)
+    await asyncio.wait(download_tasks)
 
 
 """
@@ -156,18 +151,18 @@ async def download_bucket(bucket, loop):
 """
 
 
-def upload(src, bucket, tasks, loop):
+async def upload(src, bucket, client):
     """
     Функция получает список всех файлов из всех подпапок.
     Формирует задания на загрузку файлов в хранилице.
     :param src: Путь до директории, которая должна быть загружена в хрнаилище
     :param bucket: Бакет, в который будет помещен каталог
-    :param tasks: Список заданий для асинхронного цикла
-    :param loop: Сам цикл
+    :param client:
     :return:
     """
-    if not os.path.isdir(src):
-        print("{} is not a directory".format(src))
+    # Семафор ограничивает, потребляемые корутинами ресурсы
+    sem = asyncio.Semaphore(concurrency)
+    upload_tasks = list()
 
     src_full = os.path.abspath(src)
     one_level_upper = os.path.abspath(os.path.join(src_full, "../"))
@@ -177,48 +172,42 @@ def upload(src, bucket, tasks, loop):
         for file in files:
             in_file_name = os.path.join(path, file)
             out_file_name = os.path.join(key, file)
-            tasks.append(asyncio.ensure_future(upload_one_file(in_file_name, out_file_name, bucket, loop)))
+            upload_tasks.append(asyncio.ensure_future(upload_one_file(in_file_name, out_file_name, bucket, client, sem)))
+
+    await asyncio.wait(upload_tasks)
 
 
-def download(bucket, tasks, loop):
+@error_handler
+async def async_main(login, password, command, src, dest, bucket, loop):
     """
-    Функция для скачивания бакета.
-    Формирует одно задание для цикла.
-    Распараллеливание скачивания осуществляется внутри этого задания.
-    :param bucket: Название бакета.
-    :param tasks:
-    :param loop:
-    :return:
-    """
-    tasks.append(asyncio.ensure_future(download_bucket(bucket, loop)))
-
-
-def get(src, dest, bucket, tasks, loop):
-    """
-    Функция формирует задание на скачивание одного файла.
-    :param src: Путь до файла в бакете
-    :param dest: Директория, куда сохранить файл
+    Асинхронная функция, обрабатывающая команды
+    :param login: Логин
+    :param password: Пароль
+    :param command: Команда
+    :param src: /path/to/src
+    :param dest: /path/to/dest
     :param bucket: Название бакета
-    :param tasks:
-    :param loop:
+    :param loop: Асинхронный цикл
     :return:
     """
-    if not os.path.isdir(dest):
-        sys.exit("{} is not directory".format(dest))
 
-    out_file_name = os.path.join(dest, os.path.basename(src))
-    tasks.append(asyncio.ensure_future(download_one_file(src, out_file_name, bucket, loop)))
+    session = aiobotocore.get_session(loop=loop)
+    async with session.create_client("s3",
+                                     endpoint_url=storage_url,
+                                     region_name='us-west-2',
+                                     aws_secret_access_key=password,
+                                     aws_access_key_id=login) as client:
 
-
-def lst(bucket, tasks, loop):
-    """
-    Функция формирует задание на получение списка файлов в бакете
-    :param bucket: Назание бакета
-    :param tasks:
-    :param loop:
-    :return:
-    """
-    tasks.append(asyncio.ensure_future(list_bucket(bucket, loop)))
+        if command == C_DOWNLOAD:
+            await download_bucket(bucket, client)
+        elif command == C_GET:
+            await download_one_file(src, dest, bucket, client)
+        elif command == C_LIST:
+            await list_bucket(bucket, client)
+        elif command == C_UPLOAD:
+            await upload(src, bucket, client)
+        else:
+            sys.exit("Unknown command {}".format(command))
 
 
 @click.command()
@@ -231,30 +220,9 @@ def lst(bucket, tasks, loop):
 def main(login, password, command, src, dest, bucket):
     """CLI interface for Amazon S3"""
 
-    # Часто используемые переменные в разных функциях
-    global access_key
-    global secret_key
-
-    access_key = login
-    secret_key = password
-
-    # Список заданий для асинхронного цикла
-    tasks = list()
-    loop = asyncio.get_event_loop()
-
-    # На основе введённоё команды формируется список заданий
-    if command == C_DOWNLOAD:
-        download(bucket, tasks, loop)
-    elif command == C_GET:
-        get(src, dest, bucket, tasks, loop)
-    elif command == C_LIST:
-        lst(bucket, tasks, loop)
-    elif command == C_UPLOAD:
-        upload(src, bucket, tasks, loop)
-    else:
-        sys.exit("Unknown command {}".format(command))
     # Сформированный список заданий подаётся в цикл
-    loop.run_until_complete(asyncio.wait(tasks))
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(async_main(login, password, command, src, dest, bucket, loop))
 
 
 if __name__ == "__main__":
